8b6333fe6403e5093ee4caa5d3cb27242304f95c
DiogoDantas <dioogoven@gmail.com>
2016-10-15 14:40:31 -0300

    Testes para Mochila Binaria adicionado

---
+++ tests/test_mochila_binaria.cpp
@@ -0,0 +1,40 @@
+#include "../inc/mochila_binaria.hpp"
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	int T = 0;
+
+	std::cin >> T;
+
+	while(T) {
+	    int num_elements = 0;
+	    int weight = 0;
+	    std::vector<int> weights;
+	    std::vector<int> values;
+
+	    std::cin >> num_elements;
+	    std::cin >> weight;
+
+	    for (int i = 0; i < num_elements; ++i)
+	    {
+	    	int tmp = 0;
+	    	std::cin >> tmp;
+	    	weights.push_back(tmp);
+	    }
+
+	    for (int i = 0; i < num_elements; ++i)
+	    {
+	    	int tmp = 0;
+	    	std::cin >> tmp;
+	    	values.push_back(tmp);
+	    }
+
+	    std::cout << dinamica::mochila_binaria(weight, weights, values, num_elements) << std::endl;
+
+	    T--;
+	}
+
+	
+	return 0;
+}
