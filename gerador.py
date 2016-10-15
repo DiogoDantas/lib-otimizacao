61aba61f4256a4c0b2e902b9e7f09df842d49865
DiogoDantas <dioogoven@gmail.com>
2016-10-15 14:38:36 -0300

    Atualização do script para gerar os testes da Mochila Binaria

--- gerador.py
+++ gerador.py
@@ -9,13 +9,14 @@ ENTRADA = 5
 # quantidade de casos para cada algoritmo
 TESTE = 30
 # caminhos referente a entrada
-PATH = ["inputs/in_triangulo_pascal/", "inputs/in_troco_moedas/", "inputs/in_soma_subconjunto/"]
+PATH = ["inputs/in_triangulo_pascal/", "inputs/in_troco_moedas/", "inputs/in_soma_subconjunto/", "inputs/in_mochila_binaria/"]
 
 def gerar():
 	index = 0
 	opcoes = {0: input_triangulo_pascal,
 			  1: input_troco_moedas,
-			  2: input_soma_subconjuntos}
+			  2: input_soma_subconjuntos,
+			  3: input_mochila_binaria}
 	for p in PATH:
 		for i in range(ENTRADA):
 			arquivo = open(p + "00" + str(i+1) + ".in", 'w')
@@ -48,7 +49,7 @@ def input_troco_moedas(a):
 def input_soma_subconjuntos(a):
 	#gerando casos
 	for _ in range(TESTE):
-		set_num_elements = random.randint(1,100)
+		set_num_elements = random.randint(1,10)
 		a.write(str(set_num_elements) + "\n")
 		#valores aleatórios de 1 a 100
 		elements = random.sample(range(1, 1000), set_num_elements)
@@ -57,6 +58,26 @@ def input_soma_subconjuntos(a):
 			a.write(str(x) + " ")
 
 		a.write("\n" + str(random.randint(1,1000)) + "\n")
+
+def input_mochila_binaria(a):
+	#gerando casos
+	for _ in range(TESTE):
+		num_elements = random.randint(1,10)
+		a.write(str(num_elements) + "\n")
+		W = random.randint(1,100)
+		a.write(str(W) + "\n")
+		weights = random.sample(range(1,100), num_elements)
+		values = random.sample(range(1,1000), num_elements)
+
+		for x in weights:
+			a.write(str(x) + " ")
+
+		a.write("\n")
+
+		for x in values:
+			a.write(str(x) + " ")
+
+		a.write("\n")
 
 # call
 gerar()
