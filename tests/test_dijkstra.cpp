b7b937d04bf66bef9fbadf176ae920636557b90e
DiogoDantas <dioogoven@gmail.com>
2016-10-16 12:19:30 -0300

    Testes de Dijkstra adicionado

---
+++ tests/test_dijkstra.cpp
@@ -0,0 +1,40 @@
+#include "../inc/dijkstra.hpp"
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+
+    int T = 0;
+
+    std::cin >> T;
+
+    while(T) {
+        
+        int num_vertices = 0;
+        int source = 0;
+
+        std::cin >> num_vertices;
+
+        std::vector< std::vector<int> > graph(num_vertices, std::vector<int>(num_vertices));
+
+        for (int i = 0; i < num_vertices; ++i)
+        {
+        	for (int j = 0; j < num_vertices; ++j)
+        	{
+        		int tmp = 0;
+
+        		std::cin >> tmp;
+
+        		graph[i][j]  = tmp;
+        	}
+        }
+
+    	std::cin >> source;
+
+    	greed::dijkstra(graph,source,num_vertices);
+
+    	T--;
+    }
+  
+	return 0;
+}
